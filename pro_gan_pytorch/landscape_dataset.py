afa48fe968ee35523ec3f820c8a00d8a36e8135b
Nic Dahlquist <nicolas.dahlquist@gmail.com>
2019-01-03 21:46:14 -0800

    Parallelize download of dataset.

--- pro_gan_pytorch/landscape_dataset.py
+++ pro_gan_pytorch/landscape_dataset.py
@@ -3,28 +3,38 @@ import os
 import shutil
 import urllib.request
 
+import multiprocessing
 import Dataloader
 import tqdm
 
+_torch_home = os.path.expanduser(os.getenv('TORCH_HOME', '~/.torch'))
+_directory = os.path.expanduser(_torch_home + "/.datasets/pexels_landscapes/")
+
+
+def _download_file(filename):
+    filename = filename.decode("utf-8")
+    with open(_directory + filename, 'wb') as f:
+        url = "https://storage.googleapis.com/pexels-landscapes/" + filename
+        f.write(urllib.request.urlopen(url).read())
+
 
 def maybe_download(transforms=None):
-    torch_home = os.path.expanduser(os.getenv('TORCH_HOME', '~/.torch'))
-    directory = os.path.expanduser(torch_home + "/.datasets/pexels_landscapes/")
-    if not os.path.exists(directory):
+    if not os.path.exists(_directory):
         print("Downloading dataset.")
-        os.makedirs(directory)
+        os.makedirs(_directory)
         try:
             file_list = urllib.request.urlopen("https://storage.googleapis.com/pexels-landscapes/list.txt").read().split()
 
-            for filename in tqdm.tqdm(file_list):
-                filename = filename.decode("utf-8")
-                with open(directory + filename, 'wb') as f:
-                    url = "https://storage.googleapis.com/pexels-landscapes/" + filename
-                    f.write(urllib.request.urlopen(url).read())
+            # Download using multiple threads, and use tqdm to show progress.
+            pool = multiprocessing.Pool()
+
+            for _ in tqdm.tqdm(pool.imap_unordered(_download_file, file_list), total=len(file_list)):
+                pass
+
         except Exception as e:
-            shutil.rmtree(directory)
+            shutil.rmtree(_directory)
             raise e
-    return Dataloader.FlatDirectoryImageDataset(directory, transform=transforms)
+    return Dataloader.FlatDirectoryImageDataset(_directory, transform=transforms)
 
 
 if __name__ == "__main__":
