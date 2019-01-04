7414b0c9626a41d70a08e00381af19409926e9a6
Nic Dahlquist <nicolas.dahlquist@gmail.com>
2019-01-03 21:12:00 -0800

    Run on our cluster.

--- pro_gan_pytorch/run_on_paperspace.sh
+++ pro_gan_pytorch/run_on_paperspace.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 paperspace jobs create \
-  --container ufoym/deepo \
-  --machineType GPU+ \
-  --ports 8888:22 \
-  --command "./scripts/enable_ssh.sh && TORCH_HOME=/storage/.torch CHECKPOINTS_DIR=/storage/checkpoints SAMPLES_DIR=/artifacts/ TERM=dumb python3 -u mode_pinning.py"
+ --container ufoym/deepo \
+ --clusterId "cl89n4h5h" \
+ --ports 8888:22 \
+ --command "./scripts/enable_ssh.sh && TORCH_HOME=/storage/.torch CHECKPOINTS_DIR=/storage/checkpoints SAMPLES_DIR=/artifacts/ TERM=dumb python3 -u mode_pinning.py"
